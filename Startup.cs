7edb41d1e863af3ab0ea0c657b9a5fe7f033a3d3
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2020-08-03 20:12:30 +0300

    DB Connection String with user secrets

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,9 +30,15 @@ namespace ExpensesDashboard
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // DB Connection string user-secrets string replacement
+            var builder = new SqlConnectionStringBuilder(
+            Configuration.GetConnectionString("ExpensesDashboard"));
+            builder.UserID = Configuration["ExpensesDashboard-Db-Id"];
+            builder.Password = Configuration["ExpensesDashboard-Db-Password"];
+            
             // Use MySQL database
             services.AddDbContext<ExpensesDashContext>(opt => opt.UseSqlServer
-                (Configuration.GetConnectionString("ExpensesDashboard")));
+                (builder.ConnectionString));
                 
             services.AddControllers().AddNewtonsoftJson(s => {
                 s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
